580514e8922d7b613d0983bcec752db45956a67d
Goo Gunmo <ticonhi@naver.com>
2021-04-15 20:44:05 +0900

    add 2-stage

--- 2-STAGE/networks.py
+++ 2-STAGE/networks.py
@@ -1,26 +1,27 @@
 #  from transformers import AutoConfig, AutoModelForSequenceClassification, AutoTokenizer
-from transformers import (
-    ElectraForSequenceClassification,
-    ElectraConfig,
-    ElectraTokenizer,
-)
+import torch
+import torch.nn as nn
+from transformers import AutoConfig, AutoModel, AutoTokenizer
 
 #  from transformers import Electra
 
 
+class BertClassifier(nn.Module):
+    def __init__(self, args):
+        super().__init__()
+        config = AutoConfig.from_pretrained(args.model_name_or_path)
+
+        self.backbone = AutoModel.from_pretrained(args.model_name_or_path)
+        self.fc = nn.Linear(self.backbone.config.hidden_size, args.num_labels)
+        self.do = nn.Dropout(p=0.3)
+
+    def forward(self, **inputs):
+        x = self.backbone(**inputs)[1]  # 0: all, 1: cls
+        x = self.fc(self.do(x))
+        return x
+
+
 def load_model_and_tokenizer(args):
-    config = ElectraConfig.from_pretrained(args.model_name_or_path)
-    config.num_labels = args.num_labels
-
-    #  model = AutoModelForSequenceClassification.from_config(config)
-
-    model = ElectraForSequenceClassification.from_pretrained(
-        args.model_name_or_path, config=config
-    )
-
-    #  model.load
-    model.to(args.device)
-
-    tokenizer = ElectraTokenizer.from_pretrained(args.model_name_or_path)
-
+    model = BertClassifier(args).to(args.device)
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
     return model, tokenizer
--- 2-STAGE/train.py
+++ 2-STAGE/train.py
@@ -29,6 +29,7 @@ from database import sample_strategy, execute_query
 from utils import custom_model_save, get_auto_save_path, update_args, EarlyStopping
 from hook_slack import hook_fail_strategy, hook_fail_ray, hook_simple_text
 from inference import check_last_valid_score
+from losses import FocalLoss
 
 
 def epoch_time(start_time, end_time):
@@ -38,7 +39,7 @@ def epoch_time(start_time, end_time):
     return elapsed_mins, elapsed_secs
 
 
-def train(args, model, optimizer, scheduler, dataloader):
+def train(args, model, loss_fn, optimizer, scheduler, dataloader):
     model.train()
     epoch_loss = 0.0
 
@@ -48,11 +49,15 @@ def train(args, model, optimizer, scheduler, dataloader):
         inputs = {
             "input_ids": batch["input_ids"].to(args.device),
             "attention_mask": batch["attention_mask"].to(args.device),
-            "labels": batch["label_ids"].to(args.device),
+            #  "labels": batch["label_ids"].to(args.device),
         }
 
+        labels = batch["label_ids"].to(args.device)
+
         outputs = model(**inputs)
-        loss = outputs[0]
+        loss = loss_fn(outputs, labels)
+
+        #  loss = outputs[0]
 
         loss.backward()
         optimizer.step()
@@ -62,7 +67,7 @@ def train(args, model, optimizer, scheduler, dataloader):
     return epoch_loss / len(dataloader)
 
 
-def evaluate(args, model, dataloader):
+def evaluate(args, model, loss_fn, dataloader):
     model.eval()
     epoch_loss = 0.0
 
@@ -74,30 +79,30 @@ def evaluate(args, model, dataloader):
             inputs = {
                 "input_ids": batch["input_ids"].to(args.device),
                 "attention_mask": batch["attention_mask"].to(args.device),
-                "labels": batch["label_ids"].to(args.device),
+                #  "labels": batch["label_ids"].to(args.device),
             }
 
+            labels = batch["label_ids"].to(args.device)
+
             outputs = model(**inputs)
-            loss, preds = outputs[:2]
+            loss = loss_fn(outputs, labels)
 
-            correct_len += torch.sum(
-                inputs["labels"].squeeze() == preds.argmax(-1)
-            ).item()
+            correct_len += torch.sum(labels.squeeze() == outputs.argmax(-1)).item()
 
-            total_len += preds.size(0)
+            total_len += outputs.size(0)
             epoch_loss += loss.item()
 
     return epoch_loss / len(dataloader), correct_len / total_len
 
 
-def run(args, model, optimizer, scheduler, train_dataloader, test_dataloader):
+def run(args, model, loss_fn, optimizer, scheduler, train_dataloader, test_dataloader):
     #  es_helper = EarlyStopping(args, verbose=True)  # logging, save
 
     for epoch in range(int(args.epochs)):
         start_time = time.time()
 
-        train_loss = train(args, model, optimizer, scheduler, train_dataloader)
-        valid_loss, valid_acc = evaluate(args, model, test_dataloader)
+        train_loss = train(args, model, loss_fn, optimizer, scheduler, train_dataloader)
+        valid_loss, valid_acc = evaluate(args, model, loss_fn, test_dataloader)
 
         end_time = time.time()
         epoch_mins, epoch_secs = epoch_time(start_time, end_time)
@@ -122,10 +127,12 @@ def main(config, checkpoint_dir=None):
 
     model, tokenizer = load_model_and_tokenizer(args)  # to(args.device)
     train_dataloader, test_dataloader = load_dataloader(args, tokenizer)
+    loss_fn = FocalLoss(gamma=3)
 
     optimizer = get_optimizer(args, model)
     scheduler = get_scheduler(args, optimizer)  # 안 쓰긴 하는데
-    #  run(args, model, optimizer, scheduler, train_dataloader, test_dataloader)
+
+    #  run(args, model, loss_fn, optimizer, scheduler, train_dataloader, test_dataloader)
 
     if checkpoint_dir is not None:  # Use For PBT
         print("I'm in checkpoint_dir!!")
@@ -141,8 +148,8 @@ def main(config, checkpoint_dir=None):
     while True:
         start_time = time.time()
 
-        train_loss = train(args, model, optimizer, scheduler, train_dataloader)
-        valid_loss, valid_acc = evaluate(args, model, test_dataloader)
+        train_loss = train(args, model, loss_fn, optimizer, scheduler, train_dataloader)
+        valid_loss, valid_acc = evaluate(args, model, loss_fn, test_dataloader)
 
         es_helper(train_loss, valid_loss, valid_acc, model)
 
@@ -179,7 +186,17 @@ def debug(args, strategy):
         optimizer = get_optimizer(args, model)
         scheduler = get_scheduler(args, optimizer)
 
-        run(args, model, optimizer, scheduler, train_dataloader, test_dataloader)
+        loss_fn = FocalLoss(gamma=3)
+
+        run(
+            args,
+            model,
+            loss_fn,
+            optimizer,
+            scheduler,
+            train_dataloader,
+            test_dataloader,
+        )
 
         query = f"UPDATE STRATEGY SET STATUS = 'RUN' WHERE strategy='{strategy}'"
         execute_query(query)
@@ -267,8 +284,8 @@ if __name__ == "__main__":
     ray.init()
     args = get_args()
 
-    try:
-        raytune(args)
-    except Exception as e:
-        print(e)
-        hook_fail_ray()
+    raytune(args)
+    #  try:
+    #  except Exception as e:
+    #      print(e)
+    #      hook_fail_ray()
